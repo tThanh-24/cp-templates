b34c726ab394ecbe57bef67f072afd9c0ff3bcb4
tThanh-24 <lt.thanh008@gmail.com>
2025-11-04 23:34:42 +0700

    Main template
    Open for improvement suggestions

---
+++ Template.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define NAME "main"
+#define MULTI_TEST 0 // 1 for multi-test cases, 0 for single test case
+#define FAST_IO ios::sync_with_stdio(0); cin.tie(0);
+
+using ll = long long;
+using ld = long double;
+const ll maxN = 1e5 + 10;
+const ll MOD = 1e9 + 7;
+
+#define endl '\n'
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+
+void solve(){
+    // Sol goes here
+    
+    return;
+}
+
+int main() {
+    FAST_IO
+    if (fopen(NAME".inp", "r")) {
+        freopen(NAME".inp", "r", stdin);
+        freopen(NAME".out", "w", stdout);
+        freopen(NAME".err", "w", stderr);
+    }
+    ll t = 1;
+    if(MULTI_TEST) cin >> t;
+    while(t-->0){
+        solve();
+    }
+    cerr << 1000.0 * clock() / CLOCKS_PER_SEC << " ms\n";
+    return 0;
+}
+
+// Sample In
+
+// Sample Out
